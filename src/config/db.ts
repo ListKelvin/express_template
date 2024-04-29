d509ca1745d8b24eaf552cdda6085243cf5e1ea7
Phạm Công Minh <phamm5687@gmail.com>
2024-04-29 17:06:00 +0700

    Install dotenv and connect Mongo atlas

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,7 @@ import { MONGO_URI } from "../constant/env";
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://phamm5687:ccxqxhi6Fo9pWlug@mongodb.xqo6hjj.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB"
-    );
+    await mongoose.connect(MONGO_URI);
     console.log("Successfully connected to DB");
   } catch (error) {
     console.error("Could not connect to DB", error);
